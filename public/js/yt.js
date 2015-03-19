088ea015918d0b671a6181c46300378a585443ad
Nick Drewe <nick.drewe@gmail.com>
2015-03-19 16:58:52 +0700

    player?

---
+++ public/js/yt.js
@@ -0,0 +1,31 @@
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		height: '100%',
+		width: '100%',
+		videoId: 'Oq8XQBNHzCo',
+		playerVars: { 'autoplay': 0, 'controls': 1 },
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+function onPlayerReady(event) {
+	event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+	if (event.data == YT.PlayerState.PLAYING && !done) {
+		setTimeout(stopVideo, 6000);
+		done = true;
+	}
+}
+
+function stopVideo() {
+	player.stopVideo();
+}
