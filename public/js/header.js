3fb8a5dacdfbb10f88ae1b6734548aed7a80abe3
Matt Way <matt__way@hotmail.com>
2015-03-20 15:17:44 +1000

    three js boilerplate

---
+++ public/js/header.js
@@ -0,0 +1,33 @@
+
+var scene, camera, renderer, cube;
+
+function buildScene() {
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+
+	camera.position.z = 5;
+
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	var container = document.getElementById('art');
+	container.appendChild(renderer.domElement);
+}
+
+buildScene();
+
+function render(delta) {
+
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.1;
+
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+}
+render();
