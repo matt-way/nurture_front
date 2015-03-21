ec5400a8877ee4b8dace3d87cf2f2ffe9dbb0122
Matt Way <matt__way@hotmail.com>
2015-03-21 19:23:07 +1000

    cleaned up header code

--- public/js/header.js
+++ public/js/header.js
@@ -1,206 +1,80 @@
 
-var scene, camera, renderer, geometry, mesh;
+(function(id){
+	var contId = id;
+	// scene
+	var scene, renderer, camera;
+	// lights
+	var pointA, pointB;
+	// mesh
+	var geomerty, material, mesh;
 
-// generates an array of 3 vetex faces based on a width/height
-function generatePolygons(width, height) {
-    var options = {
-    	bleed: 5,
-    	cellsize: 5,
-    	cellpadding: 5
-    };
-    var cellsX = Math.ceil((width+options.bleed*2)/options.cellsize);
-    var cellsY = Math.ceil((height+options.bleed*2)/options.cellsize);
+	function buildScene(){
+		scene = new THREE.Scene();
 
-    var vertices = d3.range(cellsX*cellsY).map(function(d) {
-        // figure out which cell we are in
-        var col = d % cellsX;
-        var row = Math.floor(d / cellsX);
-        var x = Math.round(-options.bleed + col*options.cellsize + Math.random() * (options.cellsize - options.cellpadding*2) + options.cellpadding);
-        var y = Math.round(-options.bleed + row*options.cellsize + Math.random() * (options.cellsize - options.cellpadding*2) + options.cellpadding);
-        // return [x*cellsize, y*cellsize];
-        return [x, y]; // Populate the actual background with points
-    });
+		// ambient light
+		scene.add(new THREE.AmbientLight(0x060606));
+		// point lights
+		pointA = new THREE.PointLight(0xffffff, 0.25, 100);
+		pointA.position.z = 2;
+		scene.add(pointA);
+		pointB = new THREE.PointLight(0xffffff, 0.25, 100);
+		pointB.position.z = 2;
+		scene.add(pointB);
 
-    return d3.geom.voronoi().triangles(vertices);
-}
+		// mesh
+		geometry = new THREE.PlaneGeometry(200, 100, 60, 30);
+		var material = new THREE.MeshLambertMaterial({
+			color: 0xffffff, 
+			shading: THREE.FlatShading, 
+			overdraw: 0.5
+		});
+		scene.add(new THREE.Mesh(geometry, material));
 
-// builds a geometry object based on veronoi faces
-function createGeometry(width, height, points) {
-	var g = new THREE.Geometry();
+		// camera
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+		camera.position.z = 50;
 
-	var faces = generatePolygons(width, height);
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize(window.innerWidth, window.innerHeight);
 
-	// create each face
-	for(var i=0; i<faces.length; i++){
-		var face = faces[i];
-		for(var v=2; v>=0; v--){
-		//for(var v=0; v<3; v++){
-			var vert = face[v];
-			g.vertices.push(
-				new THREE.Vector3(vert[0], vert[1], 0)
-			);	
+		var container = document.getElementById(contId);
+		container.appendChild(renderer.domElement);
+	}
+
+	var lightMod = 0;
+
+	function animateLights(){
+		pointA.position.x = 40*Math.sin(lightMod);
+		pointA.position.y = 40*Math.cos(lightMod);
+
+		pointB.position.x = -pointA.position.x;
+		pointB.position.y = -pointA.position.y;
+		lightMod += 0.01;
+	}
+
+	function animateMesh(){
+		// go through each vert and modify
+		for(var v=0; v<geometry.vertices.length; v++){
+			var vert = geometry.vertices[v];
+			vert.z += (Math.random() * 0.5) - 0.25;
 		}
-	
-		var normal = new THREE.Vector3( 0, 0, 1 );
-
-		var start = 3*i;
-		g.faces.push( new THREE.Face3(start, start+1, start+2, normal));
+		geometry.computeFaceNormals();
+		geometry.verticesNeedUpdate = true;
 	}
-	//g.mergeVertices();
-	g.computeFaceNormals();
 
-	return g;
-}
+	function render(){
+		animateLights();
+		animateMesh();
 
-
-function messDepth(geom) {
-
-	var total = geom.vertices.length;
-	for(var i=0; i<total; i++){
-		var vert = geom.vertices[i];
-		vert.z -= Math.random()*5;
+		requestAnimationFrame(render);		
+		renderer.render(scene, camera);
 	}
-	geom.computeFaceNormals();
-	geom.verticesNeedUpdate = true;
-}
 
-var light, light2;
+	// run
+	buildScene();
+	render();
 
-// Build the scene
-function buildScene() {
-	scene = new THREE.Scene();
-
-	scene.add( new THREE.AmbientLight( 0x060606 ) );
-	light = new THREE.PointLight(0xffffff, 0.25, 100);
-	light.position.set(0, 0, 2);
-	scene.add(light);
-
-	light2 = new THREE.PointLight(0xffffff, 0.25, 100);
-	light2.position.set(0, 0, 2);
-	scene.add(light2);
-
-	geometry = new THREE.PlaneGeometry( 200, 100, 60, 30 );
-	var material = new THREE.MeshLambertMaterial( {color: 0xffffff, shading: THREE.FlatShading, overdraw: 0.5 } );
-	var plane = new THREE.Mesh( geometry, material );
-
-	//messDepth(geometry);
-
-
-	scene.add( plane );
-
-
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
-
-	/*
-	meshGeometry = createGeometry(800, 450, 100);
-
-	//messDepth(meshGeometry);
-
-
-	//var geometry = new THREE.PlaneGeometry( 5, 20, 32 );
-	//var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide, wireframe: false});
-	var material = new THREE.MeshLambertMaterial({ color: 0xffffff, side: THREE.DoubleSide });
-	mesh = new THREE.Mesh(meshGeometry, material);
-
-	mesh.position.x -= 400;
-	mesh.position.y -= 225;
-	scene.add(mesh);
-	*/
-
-	camera.position.z = 50;
-
-
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-
-	var container = document.getElementById('art');
-	container.appendChild(renderer.domElement);
-}
-
-buildScene();
-
-var x=0;
-
-function positionLight() {
-
-	light.position.x = 40*Math.sin(x);
-	light.position.y = 40*Math.cos(x);
-
-	light2.position.x = -light.position.x;
-	light2.position.y = -light.position.y;
-	x += 0.01;
-}
-
-function render(delta) {
-
-	//cube.rotation.x += 0.1;
-	//cube.rotation.y += 0.1;
-
-	//light.position.x += 0.1;
-	//light.position.y += 0.1;
-	//light.position.z += 0.1;
-
-	for(var v=0; v<geometry.vertices.length; v++){
-		var vert = geometry.vertices[v];
-		vert.z += (Math.random()*0.5) - 0.25;
-	}
-	geometry.computeFaceNormals();
-	geometry.verticesNeedUpdate = true;
-
-	positionLight();
-/*
-	for(var v=0; v<geometry.vertices.length; v++){
-		var vert = geometry.vertices[v];
-		vert.z += (Math.random() * 10) - 5;
-	}
-	geometry.verticesNeedUpdate = true;
-*/
-
-	//camera.rotation.x += 0.01;
-	//camera.updateProjectionMatrix();
-
-	requestAnimationFrame(render);
-	renderer.render(scene, camera);
-}
-render();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})('art');
 
 
 
