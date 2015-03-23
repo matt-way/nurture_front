77551020959e723730cc337b62ef94156ddf7b58
Matt Way <matt__way@hotmail.com>
2015-03-23 12:09:20 +1000

    links done

--- public/js/main.js
+++ public/js/main.js
@@ -26,10 +26,81 @@ var app = angular.module('nurture', [])
 				url: 'http://www.compressionaddict.com/posts/mind/building-conscious-machines-the-hard-problem-delusion'
 			}
 		];
+	})
+	.directive('headerArt', function(){
+		return function(scope, elem, attrs){
+			// scene
+			var scene, renderer, camera;
+			// lights
+			var pointA, pointB;
+			// mesh
+			var geomerty, material, mesh;
 
-		$scope.scrollTo = function(id) {
-			$location.hash(id);
-			$anchorScroll();
+			function buildScene(){
+				scene = new THREE.Scene();
+
+				// ambient light
+				scene.add(new THREE.AmbientLight(0x060606));
+				// point lights
+				pointA = new THREE.PointLight(0xffffff, 0.25, 100);
+				pointA.position.z = 2;
+				scene.add(pointA);
+				pointB = new THREE.PointLight(0xffffff, 0.25, 100);
+				pointB.position.z = 2;
+				scene.add(pointB);
+
+				// mesh
+				geometry = new THREE.PlaneGeometry(200, 100, 60, 30);
+				var material = new THREE.MeshLambertMaterial({
+					color: 0xffffff, 
+					shading: THREE.FlatShading, 
+					overdraw: 0.5
+				});
+				scene.add(new THREE.Mesh(geometry, material));
+
+				// camera
+				camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
+				camera.position.z = 50;
+
+				renderer = new THREE.WebGLRenderer();
+				renderer.setSize(window.innerWidth, window.innerHeight);
+
+				//var container = document.getElementById(contId);
+				elem[0].appendChild(renderer.domElement);
+			}
+
+			var lightMod = 0;
+
+			function animateLights(){
+				pointA.position.x = 40*Math.sin(lightMod);
+				pointA.position.y = 40*Math.cos(lightMod);
+
+				pointB.position.x = -pointA.position.x;
+				pointB.position.y = -pointA.position.y;
+				lightMod += 0.01;
+			}
+
+			function animateMesh(){
+				// go through each vert and modify
+				for(var v=0; v<geometry.vertices.length; v++){
+					var vert = geometry.vertices[v];
+					vert.z += (Math.random() * 0.5) - 0.25;
+				}
+				geometry.computeFaceNormals();
+				geometry.verticesNeedUpdate = true;
+			}
+
+			function render(){
+				animateLights();
+				animateMesh();
+
+				requestAnimationFrame(render);		
+				renderer.render(scene, camera);
+			}
+
+			// run
+			buildScene();
+			render();
 		};
 	})
 	.directive('backImg', function(){
@@ -93,7 +164,7 @@ var app = angular.module('nurture', [])
 	                // listen for a click
 	                $element.on('click', function() {
 	                    // set the hash like a normal anchor scroll
-	                    $location.hash($scope.anchorSmoothScroll);
+	                    //$location.hash($scope.anchorSmoothScroll);
 	 
 	                    // smooth scroll to the passed in element
 	                    scrollTo($scope.anchorSmoothScroll);
